c99e0fbf0523f8cac0945ff9355b91d479da9785
sussy_tech <sussytech.techtok@gmail.com>
2025-03-12 15:23:20 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://github.com/dariotrombello/android_device_samsung_j7elte -b fox_9.0"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="j7elte"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=sussy_tech"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
